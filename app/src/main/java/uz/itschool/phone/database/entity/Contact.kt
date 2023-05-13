4178249b15ae7b24df97897da4f7c538f7f4d5c4
PC676 <rmuhammadqodir58@gmail.com>
2023-05-13 10:45:12 +0500

    1

--- app/src/main/java/uz/itschool/phone/database/entity/Contact.kt
+++ app/src/main/java/uz/itschool/phone/database/entity/Contact.kt
@@ -7,7 +7,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "contacts")
 data class Contact(
     @PrimaryKey(autoGenerate = true)
-    var id: Int,
+    var id: Int=0,
     @ColumnInfo("first_name")
     var firstName: String,
     @ColumnInfo("last_name")
--- app/src/main/java/uz/itschool/phone/screen/ContactFragment.kt
+++ app/src/main/java/uz/itschool/phone/screen/ContactFragment.kt
@@ -1,11 +1,14 @@
 package uz.itschool.phone.screen
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import uz.itschool.phone.R
 import uz.itschool.phone.adapter.ContactRecyclerAdapter
 import uz.itschool.phone.database.AppDatabase
@@ -20,9 +23,12 @@ class ContactFragment : Fragment() {
         val binding = FragmentContactBinding.inflate(inflater, container, false)
         val adapter = ContactRecyclerAdapter(appDatabase, object : ContactRecyclerAdapter.OnCall{
             override fun call(number: String) {
-                // TODO: CALL
+                val intent = Intent(Intent.ACTION_CALL);
+                intent.data = Uri.parse("tel:$number")
+                startActivity(intent)
             }
         })
+        binding.contactRecycler.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
         binding.contactRecycler.adapter = adapter
         binding.contactAddButton.setOnClickListener {
             findNavController().navigate(R.id.action_bodyFragment_to_addContactFragment)
